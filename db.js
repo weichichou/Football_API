81da6022f498e88c038230f0b8bb64fa9fb049fe
weichichou <weichi.cc@gmail.com>
2019-11-06 23:32:04 +0100

    revert the force update

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@loca
 const db = new Sequelize(databaseUrl)
 
 db
-  .sync({force:true})
+  .sync()
   .then(() => console.log('Database schema updated'))
   .catch(console.error)
 
